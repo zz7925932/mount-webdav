cfa77a155a0addb4f7bca3e656c9415977f88583
xiaohei <xiaohei891@outlook.com>
2022-08-20 14:23:50 +0800

    CHANGE: server.sh

--- server.sh
+++ server.sh
@@ -1,5 +1,4 @@
-su
-PATH=$PATH:/data/data/com.termux/files/usr/bin/
-rclone mount WebDav:/ /storage/emulated/0/WebDav/ --cache-dir /storage/emulated/0/.tmp --allow-other --vfs-cache-mode writes --allow-non-empty --config /storage/emulated/0/.rclone/rclone.conf --daemon
+export PATH=$PATH:/$MODPATH/bin/
+rclone mount WebDav:/ /storage/emulated/0/WebDav/ --cache-dir $MODPATH/tmp --allow-other --vfs-cache-mode writes --allow-non-empty --config $MODPATH/rclone.conf --daemon
 
-echo "已成功挂载！" >>/storage/emulated/0/.rclone/log.txt
+echo "已成功挂载！" >>$MODPATH/module.log
