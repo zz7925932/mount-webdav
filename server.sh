ef82731110b0f0843ddc66eea577d7e91b385c37
xiaohei <xiaohei891@outlook.com>
2022-08-20 14:26:01 +0800

    CHANGE: ui_print

--- server.sh
+++ server.sh
@@ -1,4 +1,6 @@
+ui_print "====运行中====" >$MODPATH/module.log
+
 export PATH=$PATH:/$MODPATH/bin/
 rclone mount WebDav:/ /storage/emulated/0/WebDav/ --cache-dir $MODPATH/tmp --allow-other --vfs-cache-mode writes --allow-non-empty --config $MODPATH/rclone.conf --daemon
 
-echo "已成功挂载！" >>$MODPATH/module.log
+ui_print "已成功挂载！" >>$MODPATH/module.log
